ab41e9e0b6a31639ccf8d934304e8dbe2d63ec09
Elysa Kohrs <elysakohrs@gmail.com>
2016-05-02 15:01:28 -0400

    make History table work even though not all events create history event

--- public/history.js
+++ public/history.js
@@ -1,5 +1,7 @@
 $(document).ready(function (){
 
+	createTableBody();
+
 	$('#goals-tab').click(function(){
 		window.location.href = "/profile";
 	});
@@ -8,16 +10,19 @@ $(document).ready(function (){
 		window.location.href = "/earn";
 	});
 
-	var currHistoryRows = $('#history-table tr').length;
-		console.log(currHistoryRows);
-		console.log(user.history.length);
-		for (var i = 0; i < user.history.length; i++) {
-			var newRow = "<tr><td>" + user.history[i].date + "</td>";
-			newRow += "<td><img src='" + user.history[i].imageURL + "' class='history-img'></td>";
-			newRow += "<td>" + user.history[i].eventDescription + "</td>";
-			newRow += "<td>" + user.history[i].availableFundsBalance + "</td>";
-			$("#history-table-body").append(newRow);
-		}
-
 });
+
+function createTableBody() {
+	var tbody = "<tbody id='history-table-body'>";
+	var length = user.history.length;
+	for (var i = 0; i < length; i++) {
+		var newRow = "<tr><td>" + user.history[length-i-1].date + "</td>";
+		newRow += "<td><img src='" + user.history[length-i-1].imageURL + "' class='history-img'></td>";
+		newRow += "<td>" + user.history[length-i-1].eventDescription + "</td>";
+		newRow += "<td>" + user.history[length-i-1].availableFundsBalance + "</td>";
+		tbody += newRow;
+	}
+	tbody += "</tbody>";
+	$("#history-table").append(tbody);
+}
 
